a72b6e011e54b6b647bac4779633379c4f90d694
saman azadi <samanazadi1996@gmail.com>
2024-03-03 02:34:02 +0330

    Add project files.

---
+++ Snake/MainForm.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace Snake
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            snakeTimer = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // snakeTimer
+            // 
+            snakeTimer.Tick += snakeTimer_Tick;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1255, 700);
+            Name = "MainForm";
+            Text = "Snake";
+            Load += MainForm_Load;
+            KeyDown += MainForm_KeyDown;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer snakeTimer;
+    }
+}
---
+++ Snake/MainForm.cs
@@ -0,0 +1,141 @@
+
+namespace Snake
+{
+    public partial class MainForm : Form
+    {
+
+        static bool locked = false;
+        static MoveEnum? moveTo = null;
+        static List<Label> lables = [];
+        static Label food;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (locked) return;
+
+
+            MoveEnum? temp = null;
+            if (e.KeyCode == Keys.Left) temp = MoveEnum.Left;
+            if (e.KeyCode == Keys.Up) temp = MoveEnum.Up;
+            if (e.KeyCode == Keys.Down) temp = MoveEnum.Down;
+            if (e.KeyCode == Keys.Right) temp = MoveEnum.Right;
+
+            if (temp is null || ((int)temp.Value % 2 == ((int)(moveTo ?? temp.Value + 1)) % 2))
+                return;
+
+            if (temp is not null)
+                locked = true;
+
+            moveTo = temp;
+            snakeTimer.Interval = 100;
+            snakeTimer.Start();
+        }
+        Label CreateLable(int top, int left)
+        {
+
+            var lable = new Label()
+            {
+                Height = 20,
+                Width = 20,
+                BackColor = Color.Black,
+                Top = top,
+                Left = left
+            };
+            return lable;
+        }
+
+        private void snakeTimer_Tick(object sender, EventArgs e)
+        {
+            if (lables.Count() > 1)
+                for (int i = lables.Count(); i > 1; i--)
+                {
+                    var item = lables[i - 1];
+                    var lastItem = lables[i - 2];
+
+                    item.Left = lastItem.Left;
+                    item.Top = lastItem.Top;
+                }
+
+            if (moveTo.HasValue)
+            {
+                var head = lables[0];
+                if (moveTo.Value == MoveEnum.Left)
+                    head.Left -= 20;
+
+                if (moveTo.Value == MoveEnum.Right)
+                    head.Left += 20;
+
+                if (moveTo.Value == MoveEnum.Up)
+                    head.Top -= 20;
+
+                if (moveTo.Value == MoveEnum.Down)
+                    head.Top += 20;
+
+                if (lables.Any(p => p.Top == head.Top && p.Left == head.Left && p.GetHashCode() != head.GetHashCode()))
+                    RefreshGame();
+
+                if (head.Top < 0 || head.Left < 0 || head.Left >= this.Width-20 || head.Top >= this.Height-20)
+                    RefreshGame();
+
+
+            }
+            if (lables.Any())
+            {
+                var lastitem = lables.Last();
+                if (food is not null && lastitem.Top == food.Top && lastitem.Left == food.Left)
+                {
+                    Text = $"Snake ({lables.Count()})";
+                    food.BackColor = Color.Black;
+                    lables.Add(food);
+                    CreateFood();
+                    Console.Beep();
+                }
+            }
+            locked = false;
+
+        }
+
+        private void RefreshGame()
+        {
+            snakeTimer.Stop();
+            MessageBox.Show("you lose!");
+
+            foreach (var item in lables)
+                Controls.Remove(item);
+            Controls.Remove(food);
+
+            lables.Clear();
+            moveTo = null;
+            food = null;
+            MainForm_Load(null, null);
+            Text = "Snake";
+
+        }
+        private void CreateFood()
+        {
+            var random = new Random();
+            var rndleft = random.Next((this.Width - 20) / 20) * 20;
+            var rndTop = random.Next((this.Height - 20) / 20) * 20;
+            food = CreateLable(rndTop, rndleft);
+            food.BackColor = Color.Red;
+            this.Controls.Add(food);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            lables.Add(CreateLable(200, 200));
+            Controls.Add(lables[0]);
+            CreateFood();
+        }
+    }
+    enum MoveEnum
+    {
+        Left,
+        Up,
+        Right,
+        Down,
+    }
+}
